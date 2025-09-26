0e7035d5b54e6dd2bf75757d8668894d5e0b18a9
elkham07 <elkhammamedov@gmail.com>
2025-09-26 14:18:25 +0500

    feat(metrics): counters, depth tracker, CSV writer

--- src/main/java/util/CSVWriter.java
+++ src/main/java/util/CSVWriter.java
@@ -1,4 +1,20 @@
 package util;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CSVWriter {
+    private final String file;
+
+    public CSVWriter(String file) {
+        this.file = file;
+    }
+
+    public void write(String algo, int n, long time, Metrics m) {
+        try (FileWriter fw = new FileWriter(file, true)) {
+            fw.write(algo + "," + n + "," + time + "," + m.depth + "," + m.comparisons + "," + m.swaps + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
